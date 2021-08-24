b44d6dd8fe96fe05a9f6adc65bdbaaab126c2199
Puneet Behl <behlp@objectcomputing.com>
2021-08-24 19:51:34 +0530

    Remove @Autowired from Bootstrap

--- complete/grails-app/init/demo/BootStrap.groovy
+++ complete/grails-app/init/demo/BootStrap.groovy
@@ -2,12 +2,11 @@ package demo
 
 import grails.gorm.multitenancy.Tenants
 import groovy.transform.CompileStatic
-import org.springframework.beans.factory.annotation.Autowired
 
 @CompileStatic
 class BootStrap {
-    @Autowired UserService userService
-    @Autowired PlanDataService planDataService
+    UserService userService
+    PlanDataService planDataService
 
     def init = { servletContext ->
         User vector = userService.save('vector', 'secret', 'ROLE_VILLAIN')
