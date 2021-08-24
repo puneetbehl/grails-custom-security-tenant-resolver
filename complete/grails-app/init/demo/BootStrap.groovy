a03c4666a8d24928b2414140a99ec05a717fa204
Puneet Behl <behlp@objectcomputing.com>
2021-08-24 19:46:48 +0530

    Custom Boostrap Data

--- complete/grails-app/init/demo/BootStrap.groovy
+++ complete/grails-app/init/demo/BootStrap.groovy
@@ -1,13 +1,19 @@
 package demo
 
+import grails.gorm.multitenancy.Tenants
 import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Autowired
 
 @CompileStatic
 class BootStrap {
+    @Autowired UserService userService
+    @Autowired PlanDataService planDataService
 
     def init = { servletContext ->
-
-
+        User vector = userService.save('vector', 'secret', 'ROLE_VILLAIN')
+        Tenants.withId("vector") {
+            planDataService.save('Steal a Pyramid')
+        }
     }
 
     def destroy = {
